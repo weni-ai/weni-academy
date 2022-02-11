d844f56e78d85af5d4a7f6f68a1b4522984e7b29
Matheus Martins Sarmento <matheusms280100@gmail.com>
2022-02-11 14:38:40 -0300

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import '@weni/unnnic-system';
+export default {
+  name: 'app',
 }
+</script>
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style lang="scss">
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  html {
+    @media (max-width: 1080px){
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px){
+      font-size: 87.5% ;
     }
   }
-}
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: $unnnic-font-family-secondary;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-family: $unnnic-font-family-primary;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,105 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="unnnic-grid-giant">
+    <header class="unnnic-grid-span-12">
+      <unnnic-breadcrumb
+        :crumbs="[
+          { name: 'One', path: 'One' },
+          { name: 'Two', path: 'Two' },
+          { name: 'Active', path: 'Active' },
+        ]"
+      />
+    </header>
+
+    <main class="unnnic-grid-span-8">
+      <iframe
+        v-if="hasMovie"
+        class="i"
+        src="https://www.youtube.com/embed/AVNADGzXrrQ"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+
+      <div class="main-title">
+        <div class="title">
+          <h1>Noções básicas - Conhecendo o editor</h1>
+          <h2>
+            Criando um fluxo básico para coleta de dados e engajamento,
+            entendendo os princípios.
+
+            oriawroai
+
+            daoidoai
+          </h2>
+        </div>
+
+        <div class="comments">
+          <h1>COMMENTS</h1>
+          <h2>HERE</h2>
+        </div>
+      </div>
+
+      <unnnic-tab initialTab="first" :tabs="['first', 'second']">
+        <template slot="tab-head-first">Visão geral</template>
+        <template slot="tab-panel-first"> CONTENT HERE </template>
+        <template slot="tab-head-second">Comentários</template>
+        <template slot="tab-panel-second"> COMMENTS HERE </template>
+      </unnnic-tab>
+    </main>
+
+    <aside class="unnnic-grid-span-4"></aside>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      hasMovie: true,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+h1 {
+  font-size: $unnnic-font-size-title-md;
+  color: $unnnic-color-neutral-darkest;
+}
+
+h2 {
+  margin-top: $unnnic-spacing-stack-nano;
+  font-size: $unnnic-font-size-body-lg;
+  color: $unnnic-color-neutral-cloudy;
+}
+
+.main-title {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  margin: $unnnic-spacing-stack-md 0;
+
+  .title {
+    width: 100%;
+    max-width: 80%;
+    margin-right: $unnnic-spacing-stack-sm;
+  }
+
+  .comments {
+    width: 100%;
+    max-width: 20%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
   }
 }
-</script>
+
+.i {
+  width: 100%;
+  height: 492px;
+  border-radius: $unnnic-border-radius-md;
+}
+</style>
