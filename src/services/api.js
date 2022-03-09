7a990afd318240c1b1773b159e0504c19db932f8
Matheus Martins Sarmento <matheusms280100@gmail.com>
2022-03-09 16:33:30 -0300

    fix: remove node env verification

--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,4 @@ const api = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
 });
 
-if(process.env.NODE_ENV === 'development') {
-  api.defaults.headers.common['Authorization'] = process.env.VUE_APP_API_TOKEN;
-}
-
 export { api } 
