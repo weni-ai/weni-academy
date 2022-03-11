e89b3b8748365b687004e203988485a41f616d9f
Matheus Martins Sarmento <matheusms280100@gmail.com>
2022-03-11 17:01:25 -0300

    fix: docker entrypoint quotes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 JSON_STRING='window.configs = { \
-  "VUE_APP_BASE_API":'${VUE_APP_BASE_API}', \
-  "VUE_APP_PARENT_DOMAIN":'${VUE_APP_PARENT_DOMAIN}', \
+  "VUE_APP_BASE_API":"'${VUE_APP_BASE_API}'", \
+  "VUE_APP_PARENT_DOMAIN":"'${VUE_APP_PARENT_DOMAIN}'", \
 }'
 sed -i "s@// CONFIGURATIONS_PLACEHOLDER@${JSON_STRING}@" /usr/share/nginx/html/academy/index.html
 
