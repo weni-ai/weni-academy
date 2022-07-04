b2152d49ead879987c1de61d873bb815100b2879
Matheus Martins <matheusms280100@gmail.com>
2022-07-04 13:49:47 -0300

    feat(modules) add module page

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import getEnv from '@/utils/env'
 
+
+
+const token = localStorage.getItem('token');
+
 const api = axios.create({
   baseURL: getEnv('VUE_APP_BASE_API'),
 });
+
+if (token) { api.defaults.headers.common['Authorization'] = token; }
 
 export { api } 
