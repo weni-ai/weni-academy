e89796cdd6778b2d203c3473d78e7cfafc0d20ca
Natália de Assis <98845410+nataliaweni@users.noreply.github.com>
2024-01-08 19:25:45 -0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ JSON_STRING='window.configs = { \
   "VUE_APP_PARENT_DOMAIN":"'${VUE_APP_PARENT_DOMAIN}'", \
   "VUE_APP_LOGROCKET_ID":"'${VUE_APP_LOGROCKET_ID}'", \
 }'
-sed "s|\/\/CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/academy/index.html.tmpl > /tmp/index.html
+sed "s|//CONFIGURATIONS_PLACEHOLDER|${JSON_STRING}|" /usr/share/nginx/html/academy/index.html.tmpl > /tmp/index.html
 
 exec "$@"
